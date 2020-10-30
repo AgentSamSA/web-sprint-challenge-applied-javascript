bdab2924c7a31847447fc627ae454442bb07abe4
Samuel Lee <samuelxlee@gmail.com>
2020-10-30 08:22:05 -0700

    created a function to add elements to the header, as well as an api call to add elements to trending topics

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,26 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const header = document.createElement("div");
+    const date = document.createElement("span");
+    const h1Title = document.createElement("h1");
+    const temperature = document.createElement("span");
+
+    header.classList.add("header");
+    date.classList.add("date");
+    temperature.classList.add("temp");
+
+    header.appendChild(date);
+    header.appendChild(h1Title);
+    header.appendChild(temperature);
+
+    date.textContent = "MARCH 28, 2020";
+    h1Title.textContent = "Lambda Times";
+    temperature.textContent = "98°";
+
+    return header;
+}
+
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(Header());
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,15 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+axios
+.get("https://lambda-times-api.herokuapp.com/topics")
+.then(res => {
+    res.data.topics.forEach(topic => {
+        const tab = document.createElement("div");
+        tab.classList.add("tab");
+        tab.textContent = topic;
+        document.querySelector(".topics").appendChild(tab);
+    });
+})
+.catch(error => console.log("Response failed or rejected, ", error));
